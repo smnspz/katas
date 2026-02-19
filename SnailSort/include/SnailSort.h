1c8633c98956757e11127e82370bfde5da89d3ab
Simone Rosani <rsn.smn@gmail.com>
2026-02-19 01:16:22 +0100

    Started solving `SnailSort`

---
+++ SnailSort/include/SnailSort.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+
+namespace SnailSort
+{
+
+std::vector<std::vector<int>> ThreeByThree = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+}
+
+#pragma once
--- SnailSort/src/main.cpp
+++ SnailSort/src/main.cpp
@@ -1,7 +1,43 @@
+
 #include <iostream>
+
+#include "SnailSort.h"
+
+using namespace SnailSort;
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
+    // Initializations
+    size_t columnIndex, rowIndex;
+    bool hasNext = false;
+
+    // Alias to list for better readability
+    const auto &list = ThreeByThree;
+
+    for (columnIndex = 0u; columnIndex < list.size(); ++columnIndex)
+    {
+        // Check if it's the last row
+        hasNext = (columnIndex < list.size() - 1);
+
+        if (!hasNext)
+        {
+            for (rowIndex = list[columnIndex].size() - 1; rowIndex > 0; --rowIndex)
+            {
+                std::cout << list[columnIndex][rowIndex];
+            }
+        }
+        else
+        {
+            for (rowIndex = 0u; rowIndex < list[columnIndex].size(); ++rowIndex)
+            {
+                // Once it reaches end of first row, jump to last element of second row
+                std::cout << list[columnIndex][rowIndex];
+            }
+        }
+
+        // Print new line
+        std::cout << std::endl;
+    }
+
     return 0;
 }
